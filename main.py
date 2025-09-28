ebe4c4acf1b2cbc6d15b095cf34489ff327a7a7b
bilalox_billy <bilal.benmahria.up@gmail.com>
2025-09-28 10:12:27 +0000

    Finish model evaluation pipeline

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage_1_date_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.textSummarizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerTrainingPipeline
+from src.textSummarizer.pipeline.stage_4_model_evaluation_pipeline import ModelEvaluationPipeline
 
 
 logger.info("Logging is implemented...")
@@ -39,6 +40,17 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     model_trainer_pipeline=ModelTrainerTrainingPipeline()
     model_trainer_pipeline.initiate_model_training()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Evaluation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_evaluation_pipeline = ModelEvaluationPipeline()
+    model_evaluation_pipeline.initiate_model_evaluation()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
---
+++ src/textSummarizer/pipeline/stage_4_model_evaluation_pipeline.py
@@ -0,0 +1,18 @@
+from src.textSummarizer.config.configuration import ConfigurationManager
+from src.textSummarizer.components.data_ingestion import DataIngestion
+
+from src.textSummarizer.logging import logger
+
+from src.textSummarizer.components.model_evaluation import ModelEvaluation
+
+
+
+class ModelEvaluationPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_model_evaluation(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation.evaluate()
